c9c1271b467f1532f36eb0a417dbc488f7c5bb08
Di Panfilo Marco (Student Com 20) <mdipanfilo@unibz.it>
2026-05-05 00:52:09 +0200

    Make bootstrap venv recreation non-interactive

--- scripts/bootstrap_python_env.sh
+++ scripts/bootstrap_python_env.sh
@@ -9,8 +9,7 @@ require_command curl
 install_uv
 
 echo "Creating virtual environment in ${VENV_DIR}"
-uv venv "${VENV_DIR}"
+uv venv --clear "${VENV_DIR}"
 
 echo "Installing project dependencies"
 uv pip install --python "${VENV_DIR}/bin/python" -r "${ROOT_DIR}/requirements.txt" requests psycopg2-binary
-
